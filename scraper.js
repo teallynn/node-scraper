6efecf45acdadeba0d276f04648a7994734c9c88
Teal <teallynnlar@gmail.com>
2018-01-12 14:45:36 -0500

    INitial scrape and file creation skeleton.

--- scraper.js
+++ scraper.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const scrapeIt = require('scrape-it');
+const json2csv = require('json2csv');
 
 
 function checkForFolder(folder) {
@@ -8,3 +10,53 @@ function checkForFolder(folder) {
 }
 
 checkForFolder('./data/');
+
+scrapeIt("http://shirts4mike.com/shirts.php", {
+    // Fetch the shirt pages
+    shirts: {
+        listItem: "ul.products li",
+        name: "shirts",
+        data: {
+            title: {
+              selector: "img",
+              attr: "alt"
+            },
+            url: {
+                selector: "a",
+                attr: "href"
+            }
+        }
+    }
+
+}, (err, page) => {
+
+    const shirtUrls = [];
+
+
+    if (page) {
+      const shirtPages = page.shirts;
+
+      for (var i = 0; i < shirtPages.length; i += 1) {
+        var shirt = shirtPages[i];
+        var url = `http://www.shirts4mike.com/${shirt.url}`;
+        var id = shirt.url.slice(-3);
+        shirtUrls.push(url, id);
+      }
+
+      console.log(shirtUrls);
+
+      //create a CSV file
+      function createCSVFile(data) {
+        var today = new Date(Date.now());
+        var month = today.getMonth() + 1;
+        var day = today.getDate();
+        var year = today.getFullYear();
+        var fileName = './data/' + year + "-" + month + "-" + day;
+        console.log(fileName);
+        fs.writeFileSync(fileName, data)
+      };
+
+      createCSVFile('and again');
+
+    }
+});
